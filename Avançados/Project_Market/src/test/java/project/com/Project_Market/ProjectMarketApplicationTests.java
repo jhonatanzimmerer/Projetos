feb52cbd880284f6cb67a1bf37f8aae82288b8ec
jhonatanzimmerer <jhonatanzimmerer@hotmail.com>
2021-05-31 13:00:26 -0300

    Correção de alguns erro na interface

---
+++ Avançados/Project_Market/src/test/java/project/com/Project_Market/ProjectMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package project.com.Project_Market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
