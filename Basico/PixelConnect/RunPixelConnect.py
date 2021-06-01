cba42cc1c871fd32022f2dadba374cc464a7db9a
jhonatanzimmerer <jhonatanzimmerer@hotmail.com>
2021-06-01 16:48:37 -0300

    PixelConnect: Criação de metodos para encontrar os pixel; Inicio da tratativa de problemas ao encontrar pixel; inicio da tratativa dos pixels da margem superior ao ponto escolhido;

--- Basico/PixelConnect/RunPixelConnect.py
+++ Basico/PixelConnect/RunPixelConnect.py
@@ -9,10 +9,14 @@ from cls.viewImg import ViewImg
 """obj_viewimg = ViewImg("img/Syngenta.bmp")
 obj_viewimg.limparcor((0, 0, 0), (255, 255, 255))"""
 
-
+'''Carregar a imagem e passar a cor do pixel a serem interligados'''
 obj_encontrarponto = EncontrarPonto("img/Syngenta.bmp", (96, 192, 0))
+
+'''Modificar todos os pixels pretos para branco'''
 obj_encontrarponto.obj_viewimg.limparcor((0, 0, 0), (255, 255, 255))
 
+'''procurar os pontos a serem interligados'''
 obj_encontrarponto.procurarponto()
+
 """obj_encontrarponto.obj_img.start()"""
 
--- Basico/PixelConnect/cls/encontrarPonto.py
+++ Basico/PixelConnect/cls/encontrarPonto.py
@@ -8,38 +8,99 @@ class EncontrarPonto:
         self.obj_viewimg = ViewImg(imagem)
         self.corponto = cor
 
+    '''Validar se acor é igual o que procuro'''
     def validarcor(self, y, x):
         if self.obj_viewimg.obj_img.item(y, x, 0) == self.corponto[0]:
             if self.obj_viewimg.obj_img.item(y, x, 1) == self.corponto[1]:
                 if self.obj_viewimg.obj_img.item(y, x, 2) == self.corponto[2]:
                     return True
         return False
-
+    '''Encontrar o primeiro pixel que contem a cor procurada'''
     def pontoinicio(self, altura, largura):
         for y in range(0, altura):
             for x in range(0, largura):
                 validar = self.validarcor(y, x)
                 if validar:
                     return y, x
-
+    '''Identificar o ponto mais proximo'''
     def procurarponto(self):
+        tentativa = 0
+        resultado = []
+        ponto = []
         altura, largura, canais = self.obj_viewimg.obj_img.shape
         posicao = self.pontoinicio(altura, largura)
-        """print(posicao[0], posicao[1])"""
+        if posicao[0]>0:
+            resultado.append(self.superior(posicao[0], posicao[1], tentativa, largura))
+        resultado.append(self.direita(posicao[0], posicao[1]))
+        resultado.append(self.inferior(posicao[0], posicao[1]))
+        resultado.append(self.esquerda(posicao[0], posicao[1]))
 
-    def superior(self, y, x):
-        y = y-1
-        
-        validar = self.validarcor(y, x)
+        for linha in range(0, int(len(resultado))):
+            if not resultado[linha] == (0, 0):
+                ponto.append(resultado[linha])
 
+        if len(ponto)<=0:
+            tentativa += 1
+        else:
+            pass
+            """Encontrei o ponto"""
+    '''Fazer a varredura nos pixels a cima do ponto indicado'''
+    def superior(self, y, x, tentativa, largura):
+        ponto = [y,x]
+        y -= 1
+        inicio = 0
+        fim = 0
 
-    def direita(self):
+        if x > 0:
+            inicio = x - (1 + tentativa)
+            if inicio <= 0:
+                inicio = 0
+        if x < largura:
+            fim = x + (1 + tentativa)
+            if fim > largura:
+                fim = largura
+        x = fim
 
-    def inferior(self):
+        for repetir in range(0,fim):
+            validar = self.validarcor(y, x)
+            if validar:
+                return y, x
+            x += 1
+        return 0, 0
+    '''Fazer a varredura nos pixels a direita do ponto indicado'''
+    def direita(self, y, x):
+        x += 1
+        for repetir in range(0,1):
+            validar = self.validarcor(y, x)
+            if validar:
+                return y, x
+            x += 1
+        return 0, 0
+    '''Fazer a varredura nos pixels a baixo do ponto indicado'''
+    def inferior(self, y, x):
+        y += 1
+        x -= 1
+        for repetir in range(0, 3):
+            validar = self.validarcor(y, x)
+            if validar:
+                return y, x
+            x += 1
+        return 0, 0
+    '''Fazer a varredura nos pixels a esquerda do ponto indicado'''
+    def esquerda(self, y, x):
+        x -= 1
+        for repetir in range(0, 1):
+            validar = self.validarcor(y, x)
+            if validar:
+                return y, x
+            x += 1
+        return 0, 0
 
-    def esquerda(self):
-
-
+    ''' 
+    def pontoproximo(self, resultado,ponto):
+        for linha in range(0, int(len(resultado))):
+            ponto[0]-resultado[linha]
+            '''
 """    def pontoproximo(self):
 
     def direita(self, altura, largura):
